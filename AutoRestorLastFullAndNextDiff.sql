4269e35b99c598d6cd9f0fcd64cbf9387dd57ced
Orlandis <fordford@ukr.net>
2019-10-15 18:06:08 +0300

    Update AutoRestorLastFullAndNextDiff.sql

--- AutoRestorLastFullAndNextDiff.sql
+++ AutoRestorLastFullAndNextDiff.sql
@@ -11,27 +11,27 @@ FROM
     JOIN
     msdb.dbo.backupset bs ON bs.media_set_id = bmf.media_set_id
 WHERE
-    bs.database_name not in ('model') --and bs.type = 'D'
-	--bs.database_name like '%Test%'
+    --bs.database_name not in ('model') --and bs.type = 'D'
+	bs.database_name in ('AdventureWorks2014')
 ORDER BY
 	bs.backup_start_date
-	
 
 select * from #test
 
 drop table #test1
 create table #test1 (db_n varchar(100), typ char(1),dt datetime, fil varchar(100))
 insert into #test1
-select * from #test where dt = (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'D') and db_n like 'AdventureWorks2014'
+
+select db_n, typ, dt, fil from #test where dt = (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'D') and db_n like 'AdventureWorks2014'
 union
-select * from #test where dt > (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'D') and typ = 'I' and db_n like 'AdventureWorks2014'
+select db_n,typ,dt,fil from #test where dt > (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'D') and dt = (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'I') and typ = 'I' and db_n like 'AdventureWorks2014'
 union
-select * from #test where dt > (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'I') and typ = 'L' and db_n like 'AdventureWorks2014'
+select db_n, typ, dt, fil from #test where dt > (select MAX(dt) from #test where db_n like 'AdventureWorks2014' and typ = 'I') and typ = 'L' and db_n like 'AdventureWorks2014'
 
 select * from #test1
 
---select * from msdb.dbo.backupset
---select * from msdb.dbo.backupfilegroup
-
 select 'RESTORE DATABASE '+db_n+' FROM DISK = '+'''' + fil + ''' NORECOVERY' 
 from #test1
+
+--select * from msdb.dbo.backupset
+--select * from msdb.dbo.backupfilegroup
